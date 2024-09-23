9b5edf00dac658c2afdcdfdc2f4576aac21b3623
simonmader17 <mail@simonmader.at>
2024-09-23 02:00:48 +0200

    nvim: telescope now also searches hidden files

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,3 +1,13 @@
+require("telescope").setup {
+	pickers = {
+		live_grep = {
+			additional_args = function(opts)
+				return { "--hidden" }
+			end
+		},
+	},
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
