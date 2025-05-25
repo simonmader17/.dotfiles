8790144d6d0f3e9993e6d83def4e6396486be23b
simonmader17 <mail@simonmader.at>
2025-05-25 15:43:29 +0200

    scripts improvements

--- scripts/my-disks.sh
+++ scripts/my-disks.sh
@@ -1,25 +1,36 @@
 #!/usr/bin/env bash
 
-total_used=0
-total_total=0
+# Prints the disk usage for mounted filesystems. It shows used and total space
+# per mount point and optionally calculates the total usage across all
+# filesystems.
+#
+# Dependencies:
+# - lsblk
+# - sort
+# - numfmt (optional)
 
-lsblk -o MOUNTPOINTS | sort |
-{
-	while read line; do
-		if [[ $line =~ ^/.*$ ]]; then
-			# out=$(df -h -BGiB "$line" | awk 'NR==2{print $3 " used out of " $2 " on " $6 " (" $5 ")"}')
-			out=$(df -h -BGB "$line" | awk 'NR==2{print $6 ": " $3 " used out of " $2 " (" $5 ")"}')
-			echo "$out"
-	
-			used=$(echo $out | awk '{print $2}' | numfmt --from=iec --suffix=B | tr -d -c 0-9)
-	    total=$(echo $out | awk '{print $6}' | numfmt --from=iec --suffix=B | tr -d -c 0-9)
-	    
-	    total_used=$(( $total_used + $used ))
-	    total_total=$(( $total_total + $total ))
+used=0
+total=0
+
+calc=false
+if command -v numfmt &>/dev/null; then
+	calc=true
+fi
+
+while IFS=' ' read -r mountpoint fsused fssize fspercentage; do
+	if [[ "$mountpoint" =~ ^/.*$ ]]; then
+		echo "$mountpoint: $fsused used out of $fssize ($fspercentage)"
+		if $calc; then
+			fsused_bytes="$(numfmt --from=iec "$fsused")"
+			fssize_bytes="$(numfmt --from=iec "$fssize")"
+			((used += fsused_bytes))
+			((total += fssize_bytes))
 		fi
-	done
-	
-	total_used=$(numfmt --to=iec --suffix=B $total_used)
-	total_total=$(numfmt --to=iec --suffix=B $total_total)
-	echo "In total $total_used used out of $total_total"
-}
+	fi
+done < <(lsblk -o 'MOUNTPOINT,FSUSED,FSSIZE,FSUSE%' | sort)
+
+if $calc; then
+	used="$(numfmt --to=iec "$used")"
+	total="$(numfmt --to=iec "$total")"
+	echo "In total $used used out of $total"
+fi
